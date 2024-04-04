e4cd8525e9bef33bbce447d7129fe9e05ead38ef
Will Walton <will@endless3dge.com>
2024-04-04 09:44:54 -0500

    Create db.js
    MongoDB database using Mongoose
    
    imported Mongoose library into the file
    connected to MongoDB
    then and catch methods to handle the promise resolution

---
+++ ww-rest-api/models/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/my_database', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+});
