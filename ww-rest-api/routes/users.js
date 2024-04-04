7132c575fdb5b098e0e1e758610842d1dcec3ebc
Will Walton <will@endless3dge.com>
2024-04-04 10:36:34 -0500

    Update users.js
    defined a get route again
    query the database
    retrieving all docs from the users collection in the db
    the then promise handles the find operation of finding the user data successfully
    if not the catch error will return a error message

--- ww-rest-api/routes/users.js
+++ ww-rest-api/routes/users.js
@@ -18,3 +18,13 @@ router.post('/', (req, res) => {
             res.status(500).json({ error: 'Error creating user' });
         });
 });
+
+router.get('/', (req, res) => {
+    User.find()
+        .then((users) => {
+            res.json(users);
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'Error fetching users' });
+        });
+});
