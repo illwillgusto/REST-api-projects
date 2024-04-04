a97233aec702166d004f4b93e34fd3aeb55099ce
Will Walton <will@endless3dge.com>
2024-04-04 09:21:23 -0500

    Update app.js
    added the get route method which is used to define the route for HTTP GET request
    
    added the callback function with the users array, two objects declared within the function
    
    the id and name are mock data reps for what will typically come from the database
    
    a response will be sent with the last code

--- ww-rest-api/app.js
+++ ww-rest-api/app.js
@@ -5,3 +5,13 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+// simple GET route, routes are URLs that clients can access to perform certain actions 
+app.get('/api/users', (req, res) => {
+    const users = [
+        { id: 1, name: 'Will Walton' },
+        { id: 2, name: 'Ray Walton' }
+    ];
+
+    res.json(users);
+})
