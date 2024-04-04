ac7cf797c58e850b49ecc53311d5483052b95760
Will Walton <will@endless3dge.com>
2024-04-04 10:24:08 -0500

    Create users.js
    set the required modules
    
    defined the POST Route
    declared the callback function to executed when a POST request hits the route
    
    created a new user instance
    
    save the user and sending a response

---
+++ ww-rest-api/routes/users.js
@@ -0,0 +1,20 @@
+// routes are URLs that clients can access to perform certain actions 
+// defining the CRUD routes below 
+
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+// Create a new user 
+router.post('/', (req, res) => {
+    const { name, email, age } = req.body;
+    const newUser = new User({ name, email, age });
+
+    newUser.save()
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'Error creating user' });
+        });
+});
